866eb8c465c48ba9093851f6af3435710f0c1e09
jamesboyd2008 <jamesboyd2008@gmail.com>
2016-01-02 07:59:56 -0800

    Add simple-substring directory.

---
+++ week-4/simple-substrings/my_solution.rb
@@ -0,0 +1,12 @@
+# Simple Substrings
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+# /la/.match(name)
+
+def welcome(address)
+  return "Welcome to California" if /CA/.match(address)
+  "You should move to California"
+end

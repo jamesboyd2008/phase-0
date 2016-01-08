e1f4a4905cad157bdd39ba310300eb139d15846d
jamesboyd2008 <jamesboyd2008@gmail.com>
2016-01-07 21:00:35 -0800

    Add pairing exercise directories

---
+++ week-5/calculate-mode/my_solution.rb
@@ -0,0 +1,42 @@
+# Calculate the mode Pairing Challenge
+
+# I worked on this challenge with:
+
+# I spent [] hours on this challenge.
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented.
+
+
+
+# 0. Pseudocode
+
+# What is the input?
+# What is the output? (i.e. What should the code return?)
+# What are the steps needed to solve the problem?
+
+
+# 1. Initial Solution
+
+
+
+
+# 3. Refactored Solution
+
+
+
+
+# 4. Reflection
+
+# Which data structure did you and your pair decide to implement and why?
+#
+# Were you more successful breaking this problem down into implementable
+# pseudocode than the last with a pair?
+#
+# What issues/successes did you run into when translating your pseudocode to code?
+#
+# What methods did you use to iterate through the content? Did you find any good
+# ones when you were refactoring? Were they difficult to implement?
+# 
+#
---
+++ week-5/pad-array/my_solution.rb
@@ -0,0 +1,54 @@
+# Pad an Array
+
+# I worked on this challenge with:
+
+# I spent [] hours on this challenge.
+
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented in the file.
+
+
+
+# 0. Pseudocode
+
+# What is the input?
+# What is the output? (i.e. What should the code return?)
+# What are the steps needed to solve the problem?
+
+
+# 1. Initial Solution
+def pad!(array, min_size, value = nil) #destructive
+  # Your code here
+end
+
+def pad(array, min_size, value = nil) #non-destructive
+  # Your code here
+end
+
+
+# 3. Refactored Solution
+
+
+
+# 4. Reflection
+
+# Were you successful in breaking the problem down into small steps?
+#
+# Once you had written your pseudocode, were you able to easily translate
+# it into code? What difficulties and successes did you have?
+#
+# Was your initial solution successful at passing the tests?
+# If so, why do you think that is? If not, what were the errors you encountered
+# and what did you do to resolve them?
+#
+# When you refactored, did you find any existing methods in Ruby to clean up your
+# code?
+#
+# How readable is your solution?
+# Did you and your pair choose descriptive variable names?
+#
+# What is the difference between destructive and non-destructive methods
+# in your own words?
+#
